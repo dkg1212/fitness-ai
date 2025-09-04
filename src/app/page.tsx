87da7d47b4afc4ada476226eb5d725771d39d244
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-09-04 20:37:25 +0530

    Add Clerk authentication and VAPI integration; update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client"
+
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
